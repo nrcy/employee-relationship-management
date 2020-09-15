4260f7fe7eaedbf68b67ce63a1118c7518b8dd40
nrcy <2402506721@qq.com>
2020-09-15 17:41:13 +0800

    提交了包结构，确定了包依赖关系

---
+++ src/main/java/top/nrcynet/ErmApplication.java
@@ -0,0 +1,13 @@
+package top.nrcynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmApplication.class, args);
+	}
+
+}
